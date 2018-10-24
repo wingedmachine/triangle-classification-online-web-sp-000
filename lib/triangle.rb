acd50dc5644f1be416ab928ab24b82c77af35120
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 03:32:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,9 @@
 class Triangle
-  def initialize(side1, side2, side3)
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3) 
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
+  
 end
