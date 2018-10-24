c06022553578111f65c76241ddea29b1262d0e4b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 03:34:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,16 @@
 class Triangle
-  attr_reader :side1, :side2, :side3
+  attr_reader :sides
   def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    @sides = [side1, side2, side3]
   end
 
+  def kind
+    case @sides.uniq.count
+    when 1
+      return :equilateral
+    when 2
+      return :isosceles
+    when 3
+      return :scalene
+  end
 end
